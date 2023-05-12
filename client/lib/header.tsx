6575a89193c6b5e4a2e143090850e4659da5c693
gamadjg <gamadavid36@gmail.com>
2023-05-11 23:29:10 -0700

    Moved previous page & layout into app/dashboard folder. Updated layout to be Layout and not RootLayout. Commented out header and footer components within dashboard/page and moved into their own component files under /lib. Created new RootLayout and edited tailwind classes to adjust footer to bottom of page while not using h-screen within a nested component.

---
+++ client/lib/header.tsx
@@ -0,0 +1,28 @@
+export default function Header({ loggedIn }: { loggedIn: boolean }) {
+    /*
+    Boolean prop 'loggedIn' allows for same header component 
+        to be used across site
+    */
+    return (
+        <>
+            {loggedIn ? (
+                <header className="flex justify-between items-center w-full bg-white">
+                    <div className="font-bold text-2xl p-4">Milestone</div>
+                    <div className="text-blue-400 p-4">
+                        <a href="/logout">Logout</a>
+                    </div>
+                </header>
+            ) : (
+                <header className="flex justify-between items-center w-full bg-white">
+                    <div className="font-bold text-2xl p-4">Milestone</div>
+                    <div className="text-blue-400 p-4">
+                        <a className="mr-4" href="/login">
+                            Login
+                        </a>
+                        <a href="/register">Register</a>
+                    </div>
+                </header>
+            )}
+        </>
+    );
+}
