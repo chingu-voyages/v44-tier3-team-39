293ef264038f4b94b5858789db96927d4c87a2d0
gamadjg <gamadavid36@gmail.com>
2023-05-12 19:17:51 -0700

    Text responsive sizing complete. Standard resolution (1200*1000) working on image. Button/1440p pending.

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Libre_Baskerville } from "next/font/google";
+import handPic from "../public/hand holding phone UPDATED without button.png";
 
 const libreBaskerville = Libre_Baskerville({
     weight: "700",
@@ -9,13 +10,18 @@ const libreBaskerville = Libre_Baskerville({
 
 export default function Landing() {
     return (
-        <div className="w-full h-full flex flex-row gap-10">
-            <div className="flex flex-col items-center justify-center mx-auto text-center gap-2">
-                <div className={libreBaskerville.className + " text-5xl mb-5"}>
+        <div className="w-full h-full flex items-center justify-center">
+            <div className="w-1/2 flex flex-1 flex-col items-center justify-center mx-auto text-center gap-2">
+                <div
+                    className={
+                        libreBaskerville.className +
+                        " md:text-6xl 2xl:text-7xl text-4xl mb-5 text-[#334155]"
+                    }
+                >
                     Welcome to
                     <p className="text-blue-600">Milestoned</p>
                 </div>
-                <p className="w-3/4">
+                <p className="w-1/2 md:text-lg 2xl:text-2xl">
                     With Milestoned, you can create personalized milestones for
                     your accomplishments, and easily filter them by tags.
                     Whether you want to remember a major career breakthrough, or
@@ -24,25 +30,26 @@ export default function Landing() {
                     today with Milestoned!
                 </p>
                 <Link href="/login">
-                    <button className="rounded-lg bg-slate-700 text-white px-16 py-2">
+                    <button className="rounded-lg bg-[#334155] text-white px-16 py-2 text-center w-full">
                         LOGIN
                     </button>
                 </Link>
-                <p>
+                <p className="md:text-lg 2xl:text-2xl">
                     Don't have an account?{" "}
-                    <Link href="/register" className="text-blue-600">
+                    <Link href="/register" className="text-blue-600 ">
                         Register here!
                     </Link>
                 </p>
             </div>
-            <div className="w-full h-full flex items-center justify-center">
-                <div className="rounded-3xl border-0 w-[850px] h-[700px] border-black relative">
-                    <div className="rounded-3xl w-[583px] h-[85%] border-black bg-slate-400 z-0 absolute bottom-0"></div>
+            <div className="flex-1 flex justify-center items-center pr-10">
+                <div className="rounded-3xl border-0 border-black relative w-full h-[722px]">
+                    <div className="rounded-3xl bg-[#334155] w-full h-5/6 absolute bottom-0"></div>
                     <Image
-                        src="/../public/hand holding phone UPDATED without button.png"
-                        className="lg:block absolute object-cover object-left-top"
+                        src={handPic}
+                        className="lg:block absolute left-[0px] h-full w-full object-none object-left-top rounded-3xl "
                         alt="Hand holding phone"
-                        fill={true}
+                        width={700}
+                        height={900}
                     />
                 </div>
             </div>
