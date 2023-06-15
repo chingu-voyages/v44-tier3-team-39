c04f0fa7250f42006dc9f80f7e5f09e980068640
Ozan-sert <ozansert1993@gmail.com>
2023-06-14 22:31:50 -0400

    change cors options

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -3,6 +3,6 @@ const allowedOrigins = [
     // if you receive Cors Error while testing try inputting 'http://<your IP Address>:3000'
 
     // Site URL will go here instead of:
-    "https://www.milestoned.com",
+    "*",
 ];
 export default allowedOrigins;
